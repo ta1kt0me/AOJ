02afe6a76e56999f6a4ca4310325b9ec0966fb51
ta1kt0me <p.wadachi@gmail.com>
2016-03-05 03:32:34 +0900

    Refactor

--- 0003/0003.rb
+++ 0003/0003.rb
@@ -1,9 +1,3 @@
-inputs = []
-STDIN.gets.chomp.to_i.times.map do
-  inputs << STDIN.gets.chomp.split.map(&:to_i).sort.reverse.map{|i| i**2}
-end
-
-inputs.each do |i|
-  result = i[0] == i[1] + i[2] ? 'YES' : 'NO'
-  puts result
+Array.new(STDIN.gets.chomp.to_i) { STDIN.gets.chomp.split.map(&:to_i).sort.reverse.map { |i| i**2 } }.each do |x, y, z|
+  puts x == y + z ? 'YES' : 'NO'
 end
