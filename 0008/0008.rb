7b61cbbf21653493bc711cd8fe408f7781c7843d
ta1kt0me <p.wadachi@gmail.com>
2016-03-16 02:38:12 +0900

    Refactor to reduce loop count

--- 0008/0008.rb
+++ 0008/0008.rb
@@ -1,2 +1,2 @@
 result = (0..9).to_a.repeated_permutation(4).map { |a| a.inject(:+) }.each_with_object(Hash.new(0)) { |e, a| a[e] += 1 }
-STDIN.read.split("\n").map(&:chomp).map(&:to_i).each { |n| puts result[n] || 0 }
+STDIN.read.split("\n").map { |e| e.chomp.to_i }.each { |n| puts result[n] || 0 }
