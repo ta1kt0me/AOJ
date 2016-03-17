7d5473c1edee974cb9a8963c6eb537225313237d
ta1kt0me <p.wadachi@gmail.com>
2016-03-18 00:25:05 +0900

    Use Prime

--- 0009/0009.rb
+++ 0009/0009.rb
@@ -1,58 +1,63 @@
-# require 'benchmark/ips'
-#
-# Benchmark.ips do |x|
-#   def origin
-#     max = 199_999
-#     res = (5..max).to_a.delete_if(&:even?).each_with_object([2, 3]) do |e, a|
-#       limit = Math.sqrt(e).round
-#       lst = a.take_while { |i| i < limit }
-#       unless lst.any? { |i| e % i == 0 }
-#         a.push e
-#       end
-#     end
-#     # puts res.count
-#   end
-#
-#   def eratostenes
-#     max = 199_999
-#     lst = (2..max).select(&:odd?)
-#     result = []
-#     sqrt = Math.sqrt(max)
-#
-#     loop do
-#       elem = lst.shift
-#       result << elem
-#       break if elem >= sqrt
-#       lst.delete_if { |i| i % elem == 0 }
-#     end
-#
-#     puts (result + lst).count
-#   end
-#
-#   def prime
-#     require 'prime'
-#     max = 199_999
-#     puts Prime.each(max).count
-#   end
-#
-#   x.config(time: 10, warmup: 2)
-#   x.report('origin') { origin }
-#   x.report('eratostenes') { eratostenes }
-#   x.report('prime') { prime }
-#   x.compare!
-# end
+require 'benchmark/ips'
 
-STDIN.read.split("\n").map { |i| i.chomp.to_i }.each do |max|
-  lst = (3..max).select(&:odd?)
-  result = []
-  sqrt = Math.sqrt(max)
-
-  loop do
-    elem = lst.shift
-    result << elem
-    break if elem >= sqrt
-    lst.delete_if { |i| i % elem == 0 }
+Benchmark.ips do |x|
+  def calc
+    max = 199_999
+    res = (5..max).to_a.delete_if(&:even?).each_with_object([2, 3]) do |e, a|
+      limit = Math.sqrt(e).round
+      lst = a.take_while { |i| i < limit }
+      a.push e unless lst.any? { |i| e % i == 0 }
+    end
+    res.count
   end
 
-  puts (result + lst).unshift(2).count
+  def eratostenes
+    max = 199_999
+    lst = (2..max).to_a
+    result = []
+    sqrt = Math.sqrt(max)
+
+    loop do
+      elem = lst.shift
+      result << elem
+      break if elem >= sqrt
+      lst.delete_if { |i| i % elem == 0 }
+    end
+
+    (result + lst).count
+  end
+
+  # def eratostenes_2
+  #   max = 199_999
+  #   table = Array.new(max + 1, true)
+  #   prime_list = []
+  #   (2..max).each do |num|
+  #     prime_list << num if table[num] == true
+  #     k = num * num
+  #     while k <= max
+  #       table[k] = false
+  #       k += num
+  #     end
+  #   end
+  #
+  #   prime_list.count
+  # end
+  #
+  def prime
+    require 'prime'
+    max = 199_999
+    Prime.each(max).count
+  end
+
+  x.config(time: 10, warmup: 2)
+  x.report('calc') { calc }
+  x.report('eratostenes') { eratostenes }
+  # x.report('eratostenes_2') { eratostenes_2 }
+  x.report('prime') { prime }
+  x.compare!
+end
+
+require 'prime'
+STDIN.read.split("\n").map { |i| i.chomp.to_i }.each do |max|
+  puts Prime.each(max).count
 end
