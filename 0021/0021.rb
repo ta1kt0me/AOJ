de79eaf1dd290be0c09933f7d5f8254de166cb8d
ta1kt0me <p.wadachi@gmail.com>
2016-05-09 00:45:59 +0900

    [0021]Refactor

--- 0021/0021.rb
+++ 0021/0021.rb
@@ -1,7 +1,6 @@
 require 'bigdecimal'
 input = STDIN.read.split("\n")
-result = Array.new(input.shift.to_i) do
+input.shift.to_i.times do
   x1, y1, x2, y2, x3, y3, x4, y4 = input.shift.chomp.split.map { |e| BigDecimal.new(e) }
-  (y1 == y2 && y3 == y4) || (y2 - y1) / (x2 - x1) == (y4 - y3) / (x4 - x3) ? 'YES' : 'NO'
+  puts((y1 == y2 && y3 == y4) || (y2 - y1) / (x2 - x1) == (y4 - y3) / (x4 - x3) ? 'YES' : 'NO')
 end
-puts result
